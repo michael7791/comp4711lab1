de8339acc55d4f542421f7e4c949ae768b3aa817
michael7791 <a782347791@hotmail.com>
2016-01-15 09:09:03 -0800

    test

--- index.php
+++ index.php
@@ -11,45 +11,69 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        $position = $_GET['board'];
-        $squares = str_split($position);
 
-        if (winner('x', $sequares)) {
-            echo 'You win.';
-        } else if (winner('o', $squares)) {
-            echo 'I win.';
-        } else {
-            echo 'No winner yet';
+        class Game {
+
+            var $position;
+
+            function __construct($squares) {
+                $this->position = str_split($squares);
+            }
+
+            function display() {
+                echo '<table cols="3" style = "font-size: large; font-weight:bold">';
+                echo '<tr>';
+                for ($pos = 0; $pos < 9; $pos++) {
+                    echo '<td>-</td>';
+                    if ($pos % 3 == 2) {
+                        echo '</tr><tr>';
+                    }
+                }
+                echo '</tr>';
+                echo '</table>';
+            }
+
+            function winner($token) {
+                $result = false;
+                for ($row = 0; $row < 3; $row++) {
+                    $result = true;
+                    for ($col = 0; $col < 3; $col++) {
+                        if ($this->position[3 * $row + $col] != $token) {
+                            $result = false;
+                        } else {
+                            return $result;
+                        }
+                    }
+                }
+                for ($column = 0; $column < 3; $column++) {
+                    $result = true;
+                    for ($row = 0; $row < 3; $row++) {
+                        if ($this->position[$column + 3 * $row] != $token) {
+                            $result = false;
+                        } else {
+                            return $result;
+                        }
+                    }
+                }
+                if ($this->position[0] == $token && $this->position[4] == $token && $this->position[8] == $token) {
+                    $result = true;
+                } else if ($this->position[2] == $token && $this->position[4] == $token && $this->position[6] == $token) {
+                    $result = true;
+                }
+                return $result;
+            }
+
         }
 
-        function winner($token, $position) {
-            for ($row = 0; $row < 3; $row++) {
-                $result = true;
-                for ($col = 0; $col < 3; $col++) {
-                    if ($position[3 * $row + $col] != $token) {
-                        $result = false;
-                    } else {
-                        return $result;
-                    }
-                }
-            }
-            for ($column = 0; $column < 3; $column++) {
-                $result = true;
-                for ($row = 0; $row < 3; $row++) {
-                    if ($position[$column + 3 * $row] != $token) {
-                        $result = false;
-                    } else {
-                        return $result
-                    }
-                }
-            }
+        $squares = $_GET['board'];
+        $game = new Game($squares);
 
-            if ($position[0] == $token && $position[4] == $token && $position[8] == $token) {
-                $result = true;
-            } else if ($position[2] == $token && $position[4] == $token && $position[6] == $token) {
-                $result = true;
-            }
-            return $result;
+        if ($game->winner('x')) {
+            echo 'You win, lucky guesses';
+        } else if ($game->winner('o')) {
+            echo 'I win, muhahahaahahah';
+        } else {
+            echo 'No winners yet, but you are losing';
         }
         ?>
     </body>
