e1dbca9b5070d23178c8e0b2a5621ae8f41897c0
michael7791 <a782347791@hotmail.com>
2016-01-14 22:06:18 -0800

    test

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ and open the template in the editor.
             $what = 'geek';
             $level = 10;
             echo 'Hi, my name is '.$name,'. and i am a level'.$level.' '.$what;
+            
+            echo '<br />';
+            
+            $hoursworked = 10; 
+            $rate = 12; 
+            $total = $hoursworked * $rate;
+            echo 'You owe me'.$total;
+
         ?>
     </body>
 </html>
