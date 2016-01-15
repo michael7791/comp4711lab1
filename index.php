23db2f7d96708f687d7cde62e38df97dc5e4d056
michael7791 <a782347791@hotmail.com>
2016-01-14 22:02:13 -0800

    test

--- index.php
+++ index.php
@@ -11,16 +11,10 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-          $temp = 'Jim';
-           echo 'Hi, my name is';
-           echo $temp;
-           $temp = 'geek';
-           echo "I am a";
-           echo $temp;
-           $temp = 10;
-           echo 'My level is';
-           echo $temp;
-           echo 'hi there';
+            $name = 'Jim';
+            $what = 'geek';
+            $level = 10;
+            echo 'Hi, my name is '.$name,'. and i am a level'.$level.' '.$what;
         ?>
     </body>
 </html>
